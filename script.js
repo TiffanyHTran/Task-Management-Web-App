ec99167dda523d0eed32d700ca0e7fbcf1f5afd0
tiffytran2000 <tiffytran2000@gmail.com>
2023-11-14 16:42:49 -0800

    Added newTask function and updated index.html

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function addTask(){
+    var taskInput = document.getElementById("task-input");
+    var taskList = document.getElementById("tasl-list");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter task");
+        return;
+    }
+
+    var newTask = document.getElementById("li");
+
+    //newTask.innerHTML = ;
     
+    taskList.appendChild(newTask);
+    taskInput.value = "";
 }
+
