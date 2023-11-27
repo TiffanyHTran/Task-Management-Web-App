37234b39a8a796d67c1565fb35c57449bcbefbf3
tiffytran2000 <tiffytran2000@gmail.com>
2023-11-27 14:28:08 -0800

    updated addTask

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-function addTask(){
+function addTask() {
     var taskInput = document.getElementById("task-input");
-    var taskList = document.getElementById("tasl-list");
+    var taskList = document.getElementById("task-list");
 
     if (taskInput.value.trim() === "") {
-        alert("Please enter task");
+        alert("Please enter a task!");
         return;
     }
 
-    var newTask = document.getElementById("li");
-
-    //newTask.innerHTML = '';
-
+    var newTask = document.createElement("li");
+    newTask.innerHTML = `
+        <span>${taskInput.value}</span>
+        <button class="edit-btn" onclick="editTask(this)">Edit</button>
+        <button class="delete-btn" onclick="deleteTask(this)">Delete</button>
+    `;
 
     taskList.appendChild(newTask);
     taskInput.value = "";
 }
 
-function deleteTask(button){
+
+function deleteTask(button) {
     var taskItem = button.parentNode;
     taskItem.parentNode.removeChild(taskItem);
 }
 
-function editTask(button){
+function editTask(button) {
     var taskItem = button.parentNode;
     var taskDescription = taskItem.querySelector('span');
-    var newDescription = prompt("Edit Task", taskDescription.textContent);
+    var newDescription = prompt("Edit task:", taskDescription.textContent);
 
     if (newDescription !== null && newDescription.trim() !== "") {
         taskDescription.textContent = newDescription;
