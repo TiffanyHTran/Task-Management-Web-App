fbf0ff2b51d8f745a7f7f9012a483ca8729dc5be
Tiffany Tran <42852603+tiffytran2000@users.noreply.github.com>
2023-11-28 14:46:10 -0800

    Update script.js
    Same thing it wouldnt let me push

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
 function addTask() {
     var taskInput = document.getElementById("task-input");
-    var taskList = document.getElementById("task-list");
 
     if (taskInput.value.trim() === "") {
         alert("Please enter a task!");
         return;
     }
 
-    var newTask = document.createElement("li");
-    newTask.innerHTML = `
-        <span>${taskInput.value}</span>
-        <button class="edit-btn" onclick="editTask(this)">Edit</button>
-        <button class="delete-btn" onclick="deleteTask(this)">Delete</button>
-    `;
-
-    taskList.appendChild(newTask);
-    taskInput.value = "";
+    // Use AJAX to send the task to the server
+    $.post("save_data.php", { description: taskInput.value }, function(data) {
+        if (data === "success") {
+            // Refresh the task list after successful addition
+            fetchTasks();
+            taskInput.value = "";
+        } else {
+            alert("Failed to add task!");
+        }
+    });
 }
 
-
-function deleteTask(button) {
-    var taskItem = button.parentNode;
-    taskItem.parentNode.removeChild(taskItem);
+function deleteTask(button, taskId) {
+    // Use AJAX to send the task ID to the server for deletion
+    $.post("delete_data.php", { id: taskId }, function(data) {
+        if (data === "success") {
+            // Refresh the task list after successful deletion
+            fetchTasks();
+        } else {
+            alert("Failed to delete task!");
+        }
+    });
 }
 
-function editTask(button) {
+function editTask(button, taskId) {
     var taskItem = button.parentNode;
     var taskDescription = taskItem.querySelector('span');
     var newDescription = prompt("Edit task:", taskDescription.textContent);
 
     if (newDescription !== null && newDescription.trim() !== "") {
-        taskDescription.textContent = newDescription;
+        // Use AJAX to send the updated task description to the server
+        $.post("edit_data.php", { id: taskId, description: newDescription }, function(data) {
+            if (data === "success") {
+                // Refresh the task list after successful edit
+                fetchTasks();
+            } else {
+                alert("Failed to edit task!");
+            }
+        });
     }
 }
+
+function fetchTasks() {
+    // Use AJAX to fetch tasks from the server and update the task list
+    $.get("get_data.php", function(data) {
+        document.getElementById("task-list").innerHTML = data;
+    });
+}
+
+// Fetch tasks on page load
+$(document).ready(function() {
+    fetchTasks();
+});
